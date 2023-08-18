0be38df4af95e9856a212d40d43d43e6152a15f7
Sarah Laplace <sblaplace@gmail.com>
2023-08-18 05:43:35 +0200

    first commit

---
+++ sunflower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define PROJECT_NAME "sunflower"
+
+void lex(char* program_str) {
+    while (*program_str) {
+        
+        *(program_str++) = '0';
+    }
+}
+
+void parse() {}
+
+void eval() {}
+
+int main(int argc, char **argv) {
+    if(argc != 1) {
+        printf("%s takes no arguments.\n", argv[0]);
+        return 1;
+    }
+    printf("This is project %s.\n", PROJECT_NAME);
+    return 0;
+}
