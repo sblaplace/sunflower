9353a8e6890a848d1ef7bd700528baa5cad3ec84
Sarah Laplace <sblaplace@gmail.com>
2023-08-18 12:02:27 +0200

    Remove example test

--- tests/lextests.c
+++ tests/lextests.c
@@ -1,36 +1,18 @@
 #include "munit.h"
 #include "../src/lex.h"
 
-static void*
-test_setup(const MunitParameter params[], void* user_data) {
-  return strdup("Hello, world!");
-}
-
-static void
-test_tear_down(void* fixture) {
-  free(fixture);
-}
-
-static MunitResult test(const MunitParameter params[], void* fixture) {
-  char* str = (char*) fixture;
-  munit_assert_string_equal(str, "Hello, world!");
-  return MUNIT_OK;
-}
-
 static MunitResult test_empty_string(const MunitParameter params[], void* fixture) {
   munit_assert_string_equal(lex(""), "");
   return MUNIT_OK;
 }
 
+static MunitResult test_fn(const MunitParameter params[], void* fixture) {
+  munit_assert_string_equal(lex("fn"), "");
+  return MUNIT_OK;
+}
+
+
 MunitTest tests[] = {
-  {
-    "/my-test", /* name */
-    test, /* test */
-    test_setup, /* setup */
-    test_tear_down, /* tear_down */
-    MUNIT_TEST_OPTION_NONE, /* options */
-    NULL /* parameters */
-  },
     {
     "/lex-empty-string", /* name */
     test_empty_string, /* test */
